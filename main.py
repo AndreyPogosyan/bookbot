0572e0bde0dafed7bc7a14680c324357768a4193
AndreyPogosyan <po.andrey@outlook.com>
2025-03-10 22:43:27 -0400

    completed the sorting exercise

---
+++ main.py
@@ -0,0 +1,15 @@
+from stats import get_num_words, sorted_list
+
+
+# Main function to call the get_book_text() function
+def main():
+    total_word_count = get_num_words()
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_word_count} total words")
+    print("--------- Character Count -------")
+    return sorted_list()
+
+
+main()
---
+++ stats.py
@@ -0,0 +1,35 @@
+filepath = open("books/frankenstein.txt")
+
+
+def get_book_text(filepath):
+    content = filepath.read()
+    return content
+
+
+def get_num_words():
+    file_contents = open("books/frankenstein.txt")
+    content = get_book_text(file_contents)
+    split_words = content.split()
+    length = len(split_words)
+    return length
+
+
+def character_count():
+    book_text = get_book_text(filepath).lower()
+    characters = list(book_text)
+    new_dict = {}
+    for character in characters:
+        if character in new_dict:
+            new_dict[character] += 1
+        else:
+            new_dict[character] = 1
+    return new_dict
+
+
+def sorted_list():
+    character_dict = character_count()
+    sorted_one = dict(sorted(character_dict.items(), key=lambda item: item[1], reverse=True))
+    for key, value in sorted_one.items():
+        if key.isalpha():
+            print(f"{key}: {value}")
+    print("============= END ===============")
